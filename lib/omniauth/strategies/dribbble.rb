e5f7f4b2277e4d92fb709dbb5c16b61435da5684
Adam Spooner <adam@authenticjobs.com>
2014-05-29 22:12:19 -0400

    Initial implementation.

---
+++ lib/omniauth/strategies/dribbble.rb
@@ -0,0 +1,47 @@
+require 'omniauth-oauth2'
+
+module OmniAuth
+  module Strategies
+    class Dribbble < OmniAuth::Strategies::OAuth2
+      option :client_options, {
+        site: 'https://api.dribbble.com',
+        authorize_url: 'https://dribbble.com/oauth/authorize',
+        token_url: 'https://dribbble.com/oauth/token'
+      }
+    end
+
+    def request_phase
+      super
+    end
+
+    def callback_phase
+      super
+    end
+
+    uid { raw_info['id'].to_s }
+
+    info do
+      {
+        name: raw_info['name'],
+        email: raw_info['email'],
+        nickname: raw_info['username'],
+        location: raw_info['location'],
+        description: raw_info['bio'],
+        image: raw_info['avatar_url'],
+        urls: {
+          dribbble: raw_info['html_url'],
+          web: raw_info['links']['web'],
+          twitter: raw_info['links']['twitter']
+        }
+      }
+    end
+
+    extra do
+      { raw_info: raw_info }
+    end
+
+    def raw_info
+      @raw_info ||= access_token.get('/v1/user').parsed
+    end
+  end
+end
